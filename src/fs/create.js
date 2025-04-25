42462c13d32a10145c23af1b7dedb52fbde8d819
YuliiaZas <julia.kv101@gmail.com>
2025-04-25 18:54:27 -0400

    feat: complete tasks for file system

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import { writeFile } from 'node:fs/promises';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { fsError } from './fs-error.js';
+
 const create = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const filePath = join(__dirname, 'files', 'fresh.txt');
+
+    try {
+        await writeFile(filePath, 'I am fresh and young', { flag: 'wx' });
+    } catch (error) {
+        throw error.code === 'EEXIST' ? fsError : error;
+    }
 };
 
 await create();
--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import { readdir } from 'node:fs/promises';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { fsError } from './fs-error.js';
+
 const list = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const sourceDir = join(__dirname, 'files');
+
+    try {
+        const files = await readdir(sourceDir);
+        if (files) console.log(files);
+    } catch (error) {
+        if (error.code === 'ENOENT') throw fsError;
+        throw error;
+    }
 };
 
 await list();
