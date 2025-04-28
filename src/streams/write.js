ede8cd867d976f78e7b71a8fc132ab8f689f6293
YuliiaZas <julia.kv101@gmail.com>
2025-04-28 12:37:41 -0400

    feat: complete task for stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,23 @@
+import { createWriteStream } from 'node:fs';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+    const filePath = join(__dirname, 'files', 'fileToWrite.txt');
+    const writeStream = createWriteStream(filePath, { encoding: 'utf-8' });
+
+    writeStream.on('error', (error) => {
+        console.error('Error while writing file:', error.message);
+    });
+
+    process.stdin.pipe(writeStream);
+
+    writeStream.on('finish', () => {
+        console.log('Data has been written successfully.');
+    });
 };
 
 await write();
