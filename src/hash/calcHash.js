ed976cf04bbb05414d1a36bd94a7a30a57014d43
YuliiaZas <julia.kv101@gmail.com>
2025-04-28 11:06:21 -0400

    feat: complete task for hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+import { createHash } from 'crypto';
+import { readFile } from 'node:fs/promises';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+export const read = async (fileName) => {
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const filePath = join(__dirname, 'files', fileName);
+
+    const data = await readFile(filePath, { encoding: 'utf-8' });
+    return data;
+}
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = createHash('sha256')
+        .update(await read('fileToCalculateHashFor.txt'))
+        .digest('hex');
+    console.log(hash);
 };
 
 await calculateHash();
