c8dd3e0ff73efe5e782424ed0acfed4864a84319
YuliiaZas <julia.kv101@gmail.com>
2025-04-28 10:44:58 -0400

    feat: complete task for cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,20 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    const parsedArgs = {};
+
+    for(let i = 0; i < args.length; i++) {
+        console.log('2', args[i]);
+        if (args[i].startsWith('--')) {
+            const key = args[i].slice(2);
+            const value = args[i + 1] && !args[i + 1].startsWith('--') ? args[++i] : true;
+            parsedArgs[key] = value;
+        }
+    }
+
+    const parsedResult = Object.entries(parsedArgs)
+        .map(([key, value]) => `${key} is ${value}`)
+        .join(', ');
+    console.log(parsedResult);
 };
 
 parseArgs();
