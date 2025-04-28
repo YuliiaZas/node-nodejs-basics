3a4d66678cc2b8a8b7f298f106cd3332361fab3e
YuliiaZas <julia.kv101@gmail.com>
2025-04-28 11:25:22 -0400

    fix: handle file read error

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -2,13 +2,19 @@ import { createHash } from 'crypto';
 import { readFile } from 'node:fs/promises';
 import { dirname, join } from 'node:path';
 import { fileURLToPath } from 'node:url';
+import { fsError } from '../fs/fs-error.js';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export const read = async (fileName) => {
-    const __dirname = dirname(fileURLToPath(import.meta.url));
     const filePath = join(__dirname, 'files', fileName);
 
-    const data = await readFile(filePath, { encoding: 'utf-8' });
-    return data;
+    try {
+        return await readFile(filePath, { encoding: 'utf-8' });
+    } catch (error) {
+        if (error.code === 'ENOENT') throw fsError;
+        throw error;
+    }
 }
 
 const calculateHash = async () => {
