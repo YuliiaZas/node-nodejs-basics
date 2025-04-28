bfd3fdd45cc693757d0dd367ab33c1f8bf44fcb6
YuliiaZas <julia.kv101@gmail.com>
2025-04-28 16:08:42 -0400

    feat: complete task for compression

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,26 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { createGunzip } from 'node:zlib';
+import { pipeline } from 'node:stream/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const decompress = async () => {
-    // Write your code here 
+    const initialPath = join(__dirname, 'files', 'archive.gz');
+    const decompressedPath = join(__dirname, 'files', 'fileToCompress.txt');
+
+    try {
+        await pipeline(
+            createReadStream(initialPath),
+            createGunzip(),
+            createWriteStream(decompressedPath)
+        );
+        console.log('Decompression completed successfully');
+    } catch (error) {
+        console.error('Pipeline failed:', error.message);
+    }
 };
 
 await decompress();
